c9ec4a3ec93534a6ec997bd52435ed4572b37638
unknown <rluthra@ND1DSGH126QV0X.headstrong.com>
2013-11-14 20:15:10 +0530

    First Commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function ideaAppController($scope)
+{
+	 $scope.newVariable = "Hello People!!";
+
+	 $scope.getIdeas = function(){
+
+	 };
+
+	 $scope.getIdeaDetail = function(){
+
+	 };
+
+	 $scope.addIdea = function(){
+
+	 };
+
+	 $scope.deleteIdead = function(){
+
+	 };
+}
